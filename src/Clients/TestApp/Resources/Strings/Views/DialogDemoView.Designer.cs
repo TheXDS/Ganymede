d0f0b58e9c8a680627b3638fd547951b549993bb
César Morgan <xds_xps_ivx@hotmail.com>
2023-10-18 22:37:11 -0600

    [skip ci] fix typo

--- src/Clients/TestApp/Resources/Strings/Views/DialogDemoView.Designer.cs
+++ src/Clients/TestApp/Resources/Strings/Views/DialogDemoView.Designer.cs
@@ -8,221 +8,222 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace TheXDS.Ganymede.Resources.Strings.Views; 
-using System;
-
-
-/// <summary>
-///   Clase de recurso fuertemente tipado, para buscar cadenas traducidas, etc.
-/// </summary>
-// StronglyTypedResourceBuilder generó automáticamente esta clase
-// a través de una herramienta como ResGen o Visual Studio.
-// Para agregar o quitar un miembro, edite el archivo .ResX y, a continuación, vuelva a ejecutar ResGen
-// con la opción /str o recompile su proyecto de VS.
-[global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
-[global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-[global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-public class DialogDemoView {
+namespace TheXDS.Ganymede.Resources.Strings.Views {
+    using System;
     
-    private static global::System.Resources.ResourceManager resourceMan;
-    
-    private static global::System.Globalization.CultureInfo resourceCulture;
-    
-    [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-    internal DialogDemoView() {
-    }
     
     /// <summary>
-    ///   Devuelve la instancia de ResourceManager almacenada en caché utilizada por esta clase.
+    ///   Clase de recurso fuertemente tipado, para buscar cadenas traducidas, etc.
     /// </summary>
-    [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-    public static global::System.Resources.ResourceManager ResourceManager {
-        get {
-            if (object.ReferenceEquals(resourceMan, null)) {
-                global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("TheXDS.Ganymede.Resources.Strings.Views.DialogDemoView", typeof(DialogDemoView).Assembly);
-                resourceMan = temp;
+    // StronglyTypedResourceBuilder generó automáticamente esta clase
+    // a través de una herramienta como ResGen o Visual Studio.
+    // Para agregar o quitar un miembro, edite el archivo .ResX y, a continuación, vuelva a ejecutar ResGen
+    // con la opción /str o recompile su proyecto de VS.
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
+    [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+    [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
+    public class DialogDemoView {
+        
+        private static global::System.Resources.ResourceManager resourceMan;
+        
+        private static global::System.Globalization.CultureInfo resourceCulture;
+        
+        [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
+        internal DialogDemoView() {
+        }
+        
+        /// <summary>
+        ///   Devuelve la instancia de ResourceManager almacenada en caché utilizada por esta clase.
+        /// </summary>
+        [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
+        public static global::System.Resources.ResourceManager ResourceManager {
+            get {
+                if (object.ReferenceEquals(resourceMan, null)) {
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("TheXDS.Ganymede.Resources.Strings.Views.DialogDemoView", typeof(DialogDemoView).Assembly);
+                    resourceMan = temp;
+                }
+                return resourceMan;
             }
-            return resourceMan;
         }
-    }
-    
-    /// <summary>
-    ///   Reemplaza la propiedad CurrentUICulture del subproceso actual para todas las
-    ///   búsquedas de recursos mediante esta clase de recurso fuertemente tipado.
-    /// </summary>
-    [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-    public static global::System.Globalization.CultureInfo Culture {
-        get {
-            return resourceCulture;
+        
+        /// <summary>
+        ///   Reemplaza la propiedad CurrentUICulture del subproceso actual para todas las
+        ///   búsquedas de recursos mediante esta clase de recurso fuertemente tipado.
+        /// </summary>
+        [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
+        public static global::System.Globalization.CultureInfo Culture {
+            get {
+                return resourceCulture;
+            }
+            set {
+                resourceCulture = value;
+            }
         }
-        set {
-            resourceCulture = value;
+        
+        /// <summary>
+        ///   Busca una cadena traducida similar a Is this dialog working correctly?.
+        /// </summary>
+        public static string AskText {
+            get {
+                return ResourceManager.GetString("AskText", resourceCulture);
+            }
         }
-    }
-    
-    /// <summary>
-    ///   Busca una cadena traducida similar a Is this dialog working correctly?.
-    /// </summary>
-    public static string AskText {
-        get {
-            return ResourceManager.GetString("AskText", resourceCulture);
+        
+        /// <summary>
+        ///   Busca una cadena traducida similar a Test cancellable operation dialog.
+        /// </summary>
+        public static string CancellableOpTest {
+            get {
+                return ResourceManager.GetString("CancellableOpTest", resourceCulture);
+            }
         }
-    }
-    
-    /// <summary>
-    ///   Busca una cadena traducida similar a Test cancellable operation dialog.
-    /// </summary>
-    public static string CancellableOpTest {
-        get {
-            return ResourceManager.GetString("CancellableOpTest", resourceCulture);
+        
+        /// <summary>
+        ///   Busca una cadena traducida similar a Error.
+        /// </summary>
+        public static string Error {
+            get {
+                return ResourceManager.GetString("Error", resourceCulture);
+            }
         }
-    }
-    
-    /// <summary>
-    ///   Busca una cadena traducida similar a Error.
-    /// </summary>
-    public static string Error {
-        get {
-            return ResourceManager.GetString("Error", resourceCulture);
+        
+        /// <summary>
+        ///   Busca una cadena traducida similar a Test error message.
+        /// </summary>
+        public static string ErrorTest {
+            get {
+                return ResourceManager.GetString("ErrorTest", resourceCulture);
+            }
         }
-    }
-    
-    /// <summary>
-    ///   Busca una cadena traducida similar a Test error message.
-    /// </summary>
-    public static string ErrorTest {
-        get {
-            return ResourceManager.GetString("ErrorTest", resourceCulture);
+        
+        /// <summary>
+        ///   Busca una cadena traducida similar a This is an error message..
+        /// </summary>
+        public static string ErrorText {
+            get {
+                return ResourceManager.GetString("ErrorText", resourceCulture);
+            }
         }
-    }
-    
-    /// <summary>
-    ///   Busca una cadena traducida similar a This is an error message..
-    /// </summary>
-    public static string ErrorText {
-        get {
-            return ResourceManager.GetString("ErrorText", resourceCulture);
+        
+        /// <summary>
+        ///   Busca una cadena traducida similar a Hello world!.
+        /// </summary>
+        public static string HelloWorld {
+            get {
+                return ResourceManager.GetString("HelloWorld", resourceCulture);
+            }
         }
-    }
-    
-    /// <summary>
-    ///   Busca una cadena traducida similar a Hello world!.
-    /// </summary>
-    public static string HelloWorld {
-        get {
-            return ResourceManager.GetString("HelloWorld", resourceCulture);
+        
+        /// <summary>
+        ///   Busca una cadena traducida similar a This page contains a few demos on how the dialog service works in Ganymede.
+        ///
+        ///Ganymede implements a dialog API that can be tailored to different platforms. For WPF apps, you have access to a custom dialog system that integrates well with the program&apos;s styling and interface. It follows a principle of keeping the number of independent windows down to one, and implements a consistent theme and style for all of them.
+        ///
+        ///As this dialog system is expandable, the dialog service included in Ganymede.Wpf can incorp [resto de la cadena truncado]&quot;;.
+        /// </summary>
+        public static string HelpText {
+            get {
+                return ResourceManager.GetString("HelpText", resourceCulture);
+            }
         }
-    }
-    
-    /// <summary>
-    ///   Busca una cadena traducida similar a This page contains a few demos on how the dialog service works in Ganymede.
-    ///
-    ///Ganymede implements a dialog API that can be tailored to different platforms. For WPF apps, you have access to a custom dialog system that integrates well with the program&apos;s styling and interface. It follows a principle of keeping the number of independent windows down to one, and implements a consistent theme and style for all of them.
-    ///
-    ///As this dialog system is expandable, the dialog service included in Ganymede.Wpf can incorp [resto de la cadena truncado]&quot;;.
-    /// </summary>
-    public static string HelpText {
-        get {
-            return ResourceManager.GetString("HelpText", resourceCulture);
+        
+        /// <summary>
+        ///   Busca una cadena traducida similar a Message.
+        /// </summary>
+        public static string Message {
+            get {
+                return ResourceManager.GetString("Message", resourceCulture);
+            }
         }
-    }
-    
-    /// <summary>
-    ///   Busca una cadena traducida similar a Message.
-    /// </summary>
-    public static string Message {
-        get {
-            return ResourceManager.GetString("Message", resourceCulture);
+        
+        /// <summary>
+        ///   Busca una cadena traducida similar a Test message.
+        /// </summary>
+        public static string MessageTest {
+            get {
+                return ResourceManager.GetString("MessageTest", resourceCulture);
+            }
         }
-    }
-    
-    /// <summary>
-    ///   Busca una cadena traducida similar a Test message.
-    /// </summary>
-    public static string MessageTest {
-        get {
-            return ResourceManager.GetString("MessageTest", resourceCulture);
+        
+        /// <summary>
+        ///   Busca una cadena traducida similar a Test operation dialog.
+        /// </summary>
+        public static string OperationTest {
+            get {
+                return ResourceManager.GetString("OperationTest", resourceCulture);
+            }
         }
-    }
-    
-    /// <summary>
-    ///   Busca una cadena traducida similar a Test operation dialog.
-    /// </summary>
-    public static string OperationTest {
-        get {
-            return ResourceManager.GetString("OperationTest", resourceCulture);
+        
+        /// <summary>
+        ///   Busca una cadena traducida similar a Test question.
+        /// </summary>
+        public static string QuestionTest {
+            get {
+                return ResourceManager.GetString("QuestionTest", resourceCulture);
+            }
         }
-    }
-    
-    /// <summary>
-    ///   Busca una cadena traducida similar a Test question.
-    /// </summary>
-    public static string QuestionTest {
-        get {
-            return ResourceManager.GetString("QuestionTest", resourceCulture);
+        
+        /// <summary>
+        ///   Busca una cadena traducida similar a Test item selection.
+        /// </summary>
+        public static string SelectItemTest {
+            get {
+                return ResourceManager.GetString("SelectItemTest", resourceCulture);
+            }
         }
-    }
-    
-    /// <summary>
-    ///   Busca una cadena traducida similar a Test item selection.
-    /// </summary>
-    public static string SelectItemTest {
-        get {
-            return ResourceManager.GetString("SelectItemTest", resourceCulture);
+        
+        /// <summary>
+        ///   Busca una cadena traducida similar a Text input.
+        /// </summary>
+        public static string TestTextInput {
+            get {
+                return ResourceManager.GetString("TestTextInput", resourceCulture);
+            }
         }
-    }
-    
-    /// <summary>
-    ///   Busca una cadena traducida similar a Text input.
-    /// </summary>
-    public static string TestTextInput {
-        get {
-            return ResourceManager.GetString("TestTextInput", resourceCulture);
+        
+        /// <summary>
+        ///   Busca una cadena traducida similar a Please enter some text here..
+        /// </summary>
+        public static string TextInputPrompt {
+            get {
+                return ResourceManager.GetString("TextInputPrompt", resourceCulture);
+            }
         }
-    }
-    
-    /// <summary>
-    ///   Busca una cadena traducida similar a Please enter some text here..
-    /// </summary>
-    public static string TextInputPrompt {
-        get {
-            return ResourceManager.GetString("TextInputPrompt", resourceCulture);
+        
+        /// <summary>
+        ///   Busca una cadena traducida similar a Dialog service demo.
+        /// </summary>
+        public static string Title {
+            get {
+                return ResourceManager.GetString("Title", resourceCulture);
+            }
         }
-    }
-    
-    /// <summary>
-    ///   Busca una cadena traducida similar a Dialog service demo.
-    /// </summary>
-    public static string Title {
-        get {
-            return ResourceManager.GetString("Title", resourceCulture);
+        
+        /// <summary>
+        ///   Busca una cadena traducida similar a Warning.
+        /// </summary>
+        public static string Warning {
+            get {
+                return ResourceManager.GetString("Warning", resourceCulture);
+            }
         }
-    }
-    
-    /// <summary>
-    ///   Busca una cadena traducida similar a Warning.
-    /// </summary>
-    public static string Warning {
-        get {
-            return ResourceManager.GetString("Warning", resourceCulture);
+        
+        /// <summary>
+        ///   Busca una cadena traducida similar a Test warning message.
+        /// </summary>
+        public static string WarningTest {
+            get {
+                return ResourceManager.GetString("WarningTest", resourceCulture);
+            }
         }
-    }
-    
-    /// <summary>
-    ///   Busca una cadena traducida similar a Test warning message.
-    /// </summary>
-    public static string WarningTest {
-        get {
-            return ResourceManager.GetString("WarningTest", resourceCulture);
-        }
-    }
-    
-    /// <summary>
-    ///   Busca una cadena traducida similar a This is a warning..
-    /// </summary>
-    public static string WarningText {
-        get {
-            return ResourceManager.GetString("WarningText", resourceCulture);
+        
+        /// <summary>
+        ///   Busca una cadena traducida similar a This is a warning..
+        /// </summary>
+        public static string WarningText {
+            get {
+                return ResourceManager.GetString("WarningText", resourceCulture);
+            }
         }
     }
 }
